3b77e23f87f9758b5449e87a250a0a356151598e
Logan Kessler <logankessler@ku.edu>
2026-09-18 17:05:50 -0500

    Added borderwidth changing

---
+++ practice2_files/functions.js
@@ -0,0 +1,4 @@
+function alert_paragraph_color(){
+    document.getElementById("paragraph").style.borderWidth = document.getElementById("border_width").value;
+    //document.getElementById("paragraph").style.borderColor = "rgb(" + document.getElementById("border_R") + ", 0, 0)";
+}
